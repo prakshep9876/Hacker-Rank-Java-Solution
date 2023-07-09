b0f26e9a269b11280dbb2765901f9a5f41971eb8
PRAKSHEP GOSWAMI <prakshepgoswami@gmail.com>
2023-07-10 00:45:05 +0530

    Create Java Stdin and Stdout ll.java

---
+++ 1. Introduction/Java Stdin and Stdout ll.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+  //@Prakshep_goswami
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine();
+        String s = sc.nextLine();
+        sc.close();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
